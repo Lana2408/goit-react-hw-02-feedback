4e8904bdb61ffa01153be98f08b00381c34b10c5
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-10-01 13:46:27 +0100

    Added feedback task

--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,34 +1,13 @@
-import React, { Component } from 'react';
+import { Button } from './Feedback.styled';
 
-class Feedback extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0
-  };
-
-  handleFeedback = (category) => {
-    // Функція для обробки відгуку та оновлення стану
-    this.setState((prevState) => ({
-      [category]: prevState[category] + 1
-    }));
-  };
-
-  render() {
-    return (
-      <div>
-        <h1>Збір відгуків</h1>
-        <button onClick={() => this.handleFeedback('good')}>Добре</button>
-        <button onClick={() => this.handleFeedback('neutral')}>Нейтрально</button>
-        <button onClick={() => this.handleFeedback('bad')}>Погано</button>
-
-        <h2>Статистика:</h2>
-        <p>Добре: {this.state.good}</p>
-        <p>Нейтрально: {this.state.neutral}</p>
-        <p>Погано: {this.state.bad}</p>
-      </div>
-    );
-  }
-}
-
-export default Feedback;
+export const Feedback = ({ options, handleFeedback }) => {
+  return (
+    <>
+      {options.map(option => (
+        <Button onClick={() => handleFeedback(option)} key={option}>
+          {option}
+        </Button>
+      ))}
+    </>
+  );
+};
